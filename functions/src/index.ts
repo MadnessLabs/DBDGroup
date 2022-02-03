9abf584276c1f32b9b0a2e2afbd8f31a46841e7b
Popcorn245 <bobby@madnesslabs.net>
2022-02-02 20:24:37 -0600

    fix(): Try library to ease Discord OAuth

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
 import env from "./env";
-import httpRequest from "./units/httpRequest/httpRequest";
+import * as DiscordOauth2 from "discord-oauth2";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -9,25 +9,18 @@ export const oauth = functions.https.onRequest(async (request, response) => {
   const input = request?.body || request?.params || request?.query;
   const code = input?.code || null;
   try {
-    const oauthData = await httpRequest(
-      {
-        host: "discord.com",
-        path: "/api/oauth2/token",
-        port: 443,
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      },
-      {
-        client_id: env("discord.id"),
-        client_secret: env("discord.secret"),
-        code,
-        grant_type: "authorization_code",
-        redirect_uri: `https://deadbydaylight.group/oauth/callback`,
-        scope: "identify email",
-      }
-    );
+    const oauth = new DiscordOauth2({
+      clientId: env("discord.id"),
+      clientSecret: env("discord.secret"),
+      redirectUri: `https://deadbydaylight.group/oauth/callback`,
+    });
+    const oauthData = oauth.tokenRequest({
+      // clientId, clientSecret and redirectUri are omitted, as they were already set on the class constructor
+      refreshToken: "D43f5y0ahjqew82jZ4NViEr2YafMKhue",
+      grantType: "authorization_code",
+      scope: ["identify", "email", "guilds"],
+      code,
+    });
     console.log(oauthData);
     response.redirect("https://deadbydaylight.group/discord");
   } catch (e) {
