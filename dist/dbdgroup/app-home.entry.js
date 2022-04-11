41dded60d1b443e464aa5feaf22aef9bcaa5c4ae
Popcorn245 <bobby@madnesslabs.net>
2022-04-10 21:19:27 -0500

    fix(): Update color theme and create global styles

--- dist/dbdgroup/app-home.entry.js
+++ dist/dbdgroup/app-home.entry.js
@@ -4,7 +4,7 @@ import { i as il, g as gc, Y as Yh } from './index.esm2017-64852796.js';
 import './process-es6-6fcfb3fc.js';
 import './global-e1c7e609.js';
 
-const appHomeCss = "ion-content{--background:none;background:url(\"/assets/dbdtreeBkgd.png\");background-position:center;width:100%;background-repeat:no-repeat;background-size:cover;background-color:rgb(66, 66, 66)}fireenjin-form{max-width:650px;background-color:#3d0105;text-align:center;margin:0 auto;font-family:sans-serif;opacity:0.6;border-style:ridge;border-color:#7a7a79}#bLogo{position:fixed;height:240px;width:280px;max-width:100%;margin-left:-120px;opacity:.7}";
+const appHomeCss = "app-home ion-content{--background:none;background:url(\"/assets/dbdtreeBkgd.png\");background-position:center;width:100%;background-repeat:no-repeat;background-size:cover;background-color:rgb(66, 66, 66);position:relative}app-home #bLogo{position:fixed;height:240px;width:280px;max-width:100%;margin-left:-120px;opacity:.7}";
 
 let AppHome = class {
   constructor(hostRef) {
@@ -28,29 +28,19 @@ let AppHome = class {
   }
   render() {
     var _a, _b;
-    return (h("ion-content", null, h("ion-grid", { class: "ion-no-padding" }, h("ion-row", { style: {
-        "justify-content": "center",
-      } }, h("ion-col", { size: "8", "size-md": "2" }, h("ion-accordion-group", null, h("ion-accordion", { value: "colors", "toggle-icon": "arrow-down-circle" }, h("ion-item", { style: {
+    return (h("ion-content", null, h("ion-accordion-group", null, h("ion-accordion", { value: "colors", "toggle-icon": "arrow-down-circle" }, h("ion-item", { style: {
         "text-align": "center",
-      }, slot: "header" }, h("ion-label", null, "Links")), h("ion-list", { slot: "content" }, h("ion-item", null, h("a", { href: "https://deadbydaylight.group/tournament" }, "Tournament sign-up")), h("ion-item", null, h("a", { href: "https://deadbydaylight.group/list" }, "Tournament List")), h("ion-item", null, h("a", { href: "https://deadbydaylight.group/profile" }, "Profile")), h("ion-item", null, h("a", { href: "https://deadbydaylight.group/admin" }, "Admin")), h("ion-item", null, h("a", { href: "https://deadbydaylight.group" }, "Home"))))))), h("ion-row", { style: {
-        "justify-content": "center",
-      } }, h("ion-col", { size: "10", "size-md": "4", style: {
-        "text-align": "center",
-      } }, h("ion-card", { style: {
-        opacity: ".6",
-        "border-style": "ridge",
-        "border-color": "#7a7a79",
-      } }, h("h1", { style: {
+      }, slot: "header" }, h("ion-label", null, "Links")), h("ion-list", { slot: "content" }, h("ion-item", null, h("a", { href: "https://deadbydaylight.group/tournament" }, "Tournament sign-up")), h("ion-item", null, h("a", { href: "https://deadbydaylight.group/list" }, "Tournament List")), h("ion-item", null, h("a", { href: "https://deadbydaylight.group/profile" }, "Profile")), h("ion-item", null, h("a", { href: "https://deadbydaylight.group/admin" }, "Admin")), h("ion-item", null, h("a", { href: "https://deadbydaylight.group" }, "Home"))))), h("ion-card", { style: { maxWidth: "400px", margin: "30px auto" } }, h("h1", { style: {
         "font-family": "sans-serif",
         color: "white",
-      } }, h("b", null, "DEAD BY DAYLIGHT GROUP SIGN-UP"))), h("fireenjin-form", null, h("ion-list", null, h("ion-item", null, h("ion-label", { style: {
+      } }, h("b", null, "DEAD BY DAYLIGHT GROUP SIGN-UP")), h("fireenjin-form", null, h("ion-list", null, h("ion-item", null, h("ion-label", { style: {
         "font-size": "20px",
         "font-family": "sans-serif",
         color: "white",
       }, position: "floating" }, h("b", null, "Name")), h("ion-input", { name: "name", value: (_a = this.formData) === null || _a === void 0 ? void 0 : _a.name })), h("ion-item", null, h("ion-label", { style: {
         "font-size": "20px",
         "font-family": "sans-serif",
-      }, position: "floating" }, h("b", null, "E-mail")), h("ion-input", { name: "email", value: (_b = this.formData) === null || _b === void 0 ? void 0 : _b.email })))), h("img", { id: "bLogo", src: "/assets/dbdlogoblack.png" }))))));
+      }, position: "floating" }, h("b", null, "E-mail")), h("ion-input", { name: "email", value: (_b = this.formData) === null || _b === void 0 ? void 0 : _b.email })))))));
   }
 };
 AppHome.style = appHomeCss;
--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -38,115 +38,77 @@ export class AppHome {
   render() {
     return (
       <ion-content>
-        <ion-grid class="ion-no-padding">
-          <ion-row
-            style={{
-              "justify-content": "center",
-            }}
-          >
-            <ion-col size="8" size-md="2">
-              <ion-accordion-group>
-                <ion-accordion value="colors" toggle-icon="arrow-down-circle">
-                  <ion-item
-                    style={{
-                      "text-align": "center",
-                    }}
-                    slot="header"
-                  >
-                    <ion-label>Links</ion-label>
-                  </ion-item>
-                  <ion-list slot="content">
-                    <ion-item>
-                      <a href="https://deadbydaylight.group/tournament">
-                        Tournament sign-up
-                      </a>
-                    </ion-item>
-                    <ion-item>
-                      <a href="https://deadbydaylight.group/list">
-                        Tournament List
-                      </a>
-                    </ion-item>
-                    <ion-item>
-                      <a href="https://deadbydaylight.group/profile">
-                        Profile
-                      </a>
-                    </ion-item>
-                    <ion-item>
-                      <a href="https://deadbydaylight.group/admin">
-                        Admin
-                      </a>
-                    </ion-item>
-                    <ion-item>
-                      <a href="https://deadbydaylight.group">
-                        Home
-                      </a>
-                    </ion-item>
-                  </ion-list>
-                </ion-accordion>
-              </ion-accordion-group>
-            </ion-col>
-          </ion-row>
-          <ion-row
-            style={{
-              "justify-content": "center",
-            }}
-          >
-            <ion-col
-              size="10"
-              size-md="4"
+        <ion-accordion-group>
+          <ion-accordion value="colors" toggle-icon="arrow-down-circle">
+            <ion-item
               style={{
                 "text-align": "center",
               }}
+              slot="header"
             >
-              <ion-card
-                style={{
-                  opacity: ".6",
-                  "border-style": "ridge",
-                  "border-color": "#7a7a79",
-                }}
-              >
-                <h1
+              <ion-label>Links</ion-label>
+            </ion-item>
+            <ion-list slot="content">
+              <ion-item>
+                <a href="https://deadbydaylight.group/tournament">
+                  Tournament sign-up
+                </a>
+              </ion-item>
+              <ion-item>
+                <a href="https://deadbydaylight.group/list">Tournament List</a>
+              </ion-item>
+              <ion-item>
+                <a href="https://deadbydaylight.group/profile">Profile</a>
+              </ion-item>
+              <ion-item>
+                <a href="https://deadbydaylight.group/admin">Admin</a>
+              </ion-item>
+              <ion-item>
+                <a href="https://deadbydaylight.group">Home</a>
+              </ion-item>
+            </ion-list>
+          </ion-accordion>
+        </ion-accordion-group>
+        <ion-card style={{ maxWidth: "400px", margin: "30px auto" }}>
+          <h1
+            style={{
+              "font-family": "sans-serif",
+              color: "white",
+            }}
+          >
+            <b>DEAD BY DAYLIGHT GROUP SIGN-UP</b>
+          </h1>
+
+          <fireenjin-form>
+            <ion-list>
+              <ion-item>
+                <ion-label
                   style={{
+                    "font-size": "20px",
                     "font-family": "sans-serif",
                     color: "white",
                   }}
+                  position="floating"
                 >
-                  <b>DEAD BY DAYLIGHT GROUP SIGN-UP</b>
-                </h1>
-              </ion-card>
-              <fireenjin-form>
-                <ion-list>
-                  <ion-item>
-                    <ion-label
-                      style={{
-                        "font-size": "20px",
-                        "font-family": "sans-serif",
-                        color: "white",
-                      }}
-                      position="floating"
-                    >
-                      <b>Name</b>
-                    </ion-label>
-                    <ion-input name="name" value={this.formData?.name} />
-                  </ion-item>
-                  <ion-item>
-                    <ion-label
-                      style={{
-                        "font-size": "20px",
-                        "font-family": "sans-serif",
-                      }}
-                      position="floating"
-                    >
-                      <b>E-mail</b>
-                    </ion-label>
-                    <ion-input name="email" value={this.formData?.email} />
-                  </ion-item>
-                </ion-list>
-              </fireenjin-form>
-              <img id="bLogo" src="/assets/dbdlogoblack.png"></img>
-            </ion-col>
-          </ion-row>
-        </ion-grid>
+                  <b>Name</b>
+                </ion-label>
+                <ion-input name="name" value={this.formData?.name} />
+              </ion-item>
+              <ion-item>
+                <ion-label
+                  style={{
+                    "font-size": "20px",
+                    "font-family": "sans-serif",
+                  }}
+                  position="floating"
+                >
+                  <b>E-mail</b>
+                </ion-label>
+                <ion-input name="email" value={this.formData?.email} />
+              </ion-item>
+            </ion-list>
+          </fireenjin-form>
+        </ion-card>
       </ion-content>
     );
   }
