985e7fbf12791bcb2e5bfcd2d216f486e806f887
CJ Karkut <cjkarkut@gmail.com>
2022-04-15 14:18:56 -0500

    fix(app-detail) add avatar and tournament details popover

--- dist/dbdgroup/app-detail.entry.js
+++ dist/dbdgroup/app-detail.entry.js
@@ -1,15 +1,18 @@
 import { r as registerInstance, h } from './index-0fc14935.js';
 
-const appDetailCss = "";
+const appDetailCss = "ion-list,ion-item{background:var(--ion-color-step-50);--background:var(--ion-color-step-50)}";
 
 let AppDetail = class {
   constructor(hostRef) {
     registerInstance(this, hostRef);
   }
   render() {
-    return (h("ion-content", null, h("ion-card", null, h("ion-card-title", null, "TOURNAMENT NAME"), h("ion-card-subtitle", null, "DATE"), h("ion-card-content", null, "Tournament rules (details)")), h("ion-grid", null, h("ion-row", { style: {
+    return (h("ion-content", null, h("ion-card", null, h("ion-row", null, h("ion-col", { size: "4", "size-md": "1" }, h("ion-item", null, h("ion-thumbnail", { slot: "start", style: {
+        height: "90px",
+        width: "110px",
+      } }, h("img", { src: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" })))), h("ion-col", { size: "8", "size-md": "11" }, h("ion-card-title", null, "TOURNAMENT NAME"), h("ion-card-subtitle", null, "DATE"), h("ion-button", { size: "cover", id: "trigger-button" }, "Tournament Details"), h("ion-popover", { trigger: "trigger-button" }, h("ion-content", null, h("ul", null, h("p", null, h("li", null, "Don't Be Cringe, Don't Be Cringe, Don't Be Cringe, Don't Be Cringe")), h("p", null, h("li", null, "Don't Be Cringe")), h("p", null, h("li", null, "Don't Be Cringe")), h("p", null, h("li", null, "Don't Be Cringe")), h("p", null, h("li", null, "Don't Be Cringe")), h("p", null, h("li", null, "Don't Be Cringe")), h("p", null, h("li", null, "Don't Be Cringe")))))))), h("ion-grid", null, h("ion-row", { style: {
         "flex-wrap": "wrap-reverse",
-      } }, h("ion-col", { size: "12", "size-md": "6" }, h("ion-card", null, h("ion-item", null, h("ion-avatar", { slot: "start" }, h("img", { src: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" })), h("p", null, "SURVIVOR"))), h("ion-card", null, h("ion-item", null, h("ion-avatar", { slot: "start" }, h("img", { src: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" })), h("p", null, "SURVIVOR")))), h("ion-col", { size: "12", "size-md": "6" }, h("ion-card", null, h("ion-item", null, h("ion-avatar", { slot: "start" }, h("img", { src: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" })), h("p", null, "KILLER"))))), h("ion-row", null, h("ion-col", { size: "12", "size-md": "6" }, h("ion-card", null, h("p", null, "SIGNUP HERE")))))));
+      } }, h("ion-col", { size: "12", "size-md": "6" }, h("ion-card", null, h("ion-item", null, h("ion-avatar", { slot: "start" }, h("img", { src: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" })), h("p", null, "SURVIVOR"))), h("ion-card", null, h("ion-item", null, h("ion-avatar", { slot: "start" }, h("img", { src: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" })), h("p", null, "SURVIVOR"))), h("ion-card", null, h("ion-item", null, h("ion-avatar", { slot: "start" }, h("img", { src: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" })), h("p", null, "SURVIVOR"))), h("ion-card", null, h("ion-item", null, h("ion-avatar", { slot: "start" }, h("img", { src: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" })), h("p", null, "SURVIVOR")))), h("ion-col", { size: "12", "size-md": "6" }, h("ion-card", null, h("ion-item", null, h("ion-avatar", { slot: "start" }, h("img", { src: "https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" })), h("p", null, "KILLER"))))), h("ion-row", null, h("ion-col", { size: "12", "size-md": "6" }, h("ion-card", null, h("p", null, "SIGNUP HERE")))))));
   }
 };
 AppDetail.style = appDetailCss;
--- src/components/app-detail/app-detail.tsx
+++ src/components/app-detail/app-detail.tsx
@@ -17,12 +17,39 @@ export class AppDetail {
     return (
       <ion-content>
         <ion-card>
-          {/* <ion-thumbnail>
-            <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
-          </ion-thumbnail> */}
-          <ion-card-title>TOURNAMENT NAME</ion-card-title>
-          <ion-card-subtitle>DATE</ion-card-subtitle>
-          <ion-card-content>Tournament rules (details)</ion-card-content>
+          <ion-row>
+            <ion-col size="4" size-md="1">
+              <ion-item>
+                <ion-thumbnail
+                  slot="start"
+                  style={{
+                    height: "90px",
+                    width: "110px",
+                  }}
+                >
+                  <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
+                </ion-thumbnail>
+              </ion-item>
+            </ion-col>
+            <ion-col size="8" size-md="11">
+              <ion-card-title>TOURNAMENT NAME</ion-card-title>
+              <ion-card-subtitle>DATE</ion-card-subtitle>
+              <ion-button size="cover" id="trigger-button">Tournament Details</ion-button>
+              <ion-popover trigger="trigger-button">
+                <ion-content>
+                  <ul>
+                  <p><li>Don't Be Cringe, Don't Be Cringe, Don't Be Cringe, Don't Be Cringe</li></p>
+                  <p><li>Don't Be Cringe</li></p>
+                  <p><li>Don't Be Cringe</li></p>
+                  <p><li>Don't Be Cringe</li></p>
+                  <p><li>Don't Be Cringe</li></p>
+                  <p><li>Don't Be Cringe</li></p>
+                  <p><li>Don't Be Cringe</li></p>
+                  </ul>
+                </ion-content>
+              </ion-popover>
+            </ion-col>
+          </ion-row>
         </ion-card>
         <ion-grid>
           <ion-row
@@ -54,6 +81,22 @@ export class AppDetail {
                   <p>SURVIVOR</p>
                 </ion-item>
               </ion-card>
+              <ion-card>
+                <ion-item>
+                  <ion-avatar slot="start">
+                    <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
+                  </ion-avatar>
+                  <p>SURVIVOR</p>
+                </ion-item>
+              </ion-card>
+              <ion-card>
+                <ion-item>
+                  <ion-avatar slot="start">
+                    <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
+                  </ion-avatar>
+                  <p>SURVIVOR</p>
+                </ion-item>
+              </ion-card>
             </ion-col>
             {/* <fireenjin-pagination
               ref={(el) => (this.paginationEl = el)}
@@ -63,7 +106,7 @@ export class AppDetail {
               query={this.query}
             /> */}
             <ion-col size="12" size-md="6">
-            <ion-card>
+              <ion-card>
                 <ion-item>
                   <ion-avatar slot="start">
                     <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
