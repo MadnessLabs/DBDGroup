439e7a0c4ec35fff455aad5e9df672c9bac4aa52
Popcorn245 <bobby@madnesslabs.net>
2022-04-22 21:50:01 -0500

    fix(): Remove www dir from repo

--- dist/types/components/modals/modal-tournament-detail/modal-tournament-detail.d.ts
+++
@@ -1,8 +0,0 @@
-import { DatabaseService } from "@fireenjin/sdk";
-export declare class ModalTournamentDetail {
-  tournament: Tournament;
-  tournamentId: string;
-  rules: string[];
-  db: DatabaseService;
-  render(): any;
-}
