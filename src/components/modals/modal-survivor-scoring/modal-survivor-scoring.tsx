f2901e489219c39e656e97f52cc2af869a6a27d0
Stick Man <stickman4072@gmail.com>
2022-05-11 15:26:01 -0500

    fix(): Updated the indexing/scoring for survivors. Still no fix on them not updating the chips

--- src/components/modals/modal-survivor-scoring/modal-survivor-scoring.tsx
+++ src/components/modals/modal-survivor-scoring/modal-survivor-scoring.tsx
@@ -37,76 +37,73 @@ export class ModalSurvivorScoring {
         </ion-toolbar>
       </ion-header>,
       <ion-content>
-        <ion-row style={{
-          "justify-content":"center"
-        }}>
-          <ion-col size="12" size-md="8">
-        <fireenjin-form style={{
-          "text-align":"center"
-        }}
-          documentId={this.tournamentId}
-          endpoint="tournaments"
-          beforeSubmit={async (data) => {
-            const matches = this.tournament?.matches || [];
-            matches.push({
-              timestamp: new Date(),
-              scoring: {
-                survivor: data?.survivor || {},
-                killer: data?.killer || {},
-              },
-            });
-            const killerScores: {
-              [userId: string]: KillerScores;
-            } = {};
-            for (const match of matches) {
-              for (const [userId, scores] of Object.entries(
-                match?.scoring?.killer || {}
-              )) {
-                if (!killerScores[userId])
-                  killerScores[userId] = {
-                    escapes: 0,
-                    generatorsLeft: 0,
-                    kills: 0,
-                  };
-                killerScores[userId].escapes =
-                  killerScores[userId].escapes +
-                  parseInt((scores?.escapes as any) || "0");
-                killerScores[userId].generatorsLeft =
-                  killerScores[userId].generatorsLeft +
-                  parseInt((scores?.generatorsLeft as any) || "0");
-                killerScores[userId].kills =
-                  killerScores[userId].kills +
-                  parseInt((scores?.kills as any) || "0");
-              }
-            }
-
-            this.tournament.matches = matches;
-            for (const [userId, score] of Object.entries(killerScores)) {
-              const killerIndex = (this.tournament?.killers || []).findIndex(
-                (killer) => killer?.user?.id === userId
-              );
-              this.tournament.killers[killerIndex].scoring = score;
-            }
-
-            return this.tournament;
+        <ion-row
+          style={{
+            "justify-content": "center",
           }}
         >
-          {(this.tournament?.survivors || []).map((survivor) => (
-            <ion-card>
-              {survivor?.name || "No Name"}
-              <fireenjin-input
-                min="0"
-                type="number"
-                label="Bloodpoints"
-                labelPosition="stacked"
-                name={`survivor.${survivor?.user?.id}.bloodpoint`}
-              />
-            </ion-card>
-          ))}
-        </fireenjin-form>
-        </ion-col>
+          <ion-col size="12" size-md="8">
+            <fireenjin-form
+              style={{
+                "text-align": "center",
+              }}
+              documentId={this.tournamentId}
+              endpoint="tournaments"
+              beforeSubmit={async (data) => {
+                const matches = this.tournament?.matches || [];
+                matches.push({
+                  timestamp: new Date(),
+                  scoring: {
+                    survivor: data?.survivor || {},
+                    killer: data?.killer || {},
+                  },
+                });
+                const SurvivorScores: {
+                  [userId: string]: SurvivorScores;
+                } = {};
+                for (const match of matches) {
+                  for (const [userId, scores] of Object.entries(
+                    match?.scoring?.survivor || {}
+                  )) {
+                    if (!SurvivorScores[userId])
+                      SurvivorScores[userId] = {
+                        bloodpoints: 0,
+                      };
+                    SurvivorScores[userId].bloodpoints =
+                      SurvivorScores[userId].bloodpoints +
+                      parseInt((scores?.bloodpoints as any) || "0");
+                  }
+
+                  this.tournament.matches = matches;
+                  for (const [userId, score] of Object.entries(
+                    SurvivorScores
+                  )) {
+                    const survivorIndex = (
+                      this.tournament?.survivors || []
+                    ).findIndex((killer) => killer?.user?.id === userId);
+                    this.tournament.survivors[survivorIndex].scoring = score;
+                  }
+
+                  return this.tournament;
+                }
+              }}
+            >
+              {(this.tournament?.survivors || []).map((survivor) => (
+                <ion-card>
+                  {survivor?.name || "No Name"}
+                  <fireenjin-input
+                    min="0"
+                    type="number"
+                    label="Bloodpoints"
+                    labelPosition="stacked"
+                    name={`survivor.${survivor?.user?.id}.bloodpoints`}
+                  />
+                </ion-card>
+              ))}
+            </fireenjin-form>
+          </ion-col>
         </ion-row>
-      </ion-content>
+      </ion-content>,
     ];
   }
 }
